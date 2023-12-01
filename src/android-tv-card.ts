325117ca73315ed9951c61b34fc0cd54337ce5e9
Nerwyn Singh <nerwyn1994@gmail.com>
2023-12-01 01:31:26 -0500

    copy over renderTemplate function from service-call-tile-feature

--- src/android-tv-card.ts
+++ src/android-tv-card.ts
@@ -4,6 +4,8 @@ import { LitElement, TemplateResult, html, css } from 'lit';
 import { property } from 'lit/decorators.js';
 import { styleMap } from 'lit/directives/style-map.js';
 
+import { renderString } from 'nunjucks';
+
 import { HomeAssistant, applyThemesOnElement } from 'custom-card-helpers';
 
 import {
@@ -429,6 +431,88 @@ class AndroidTVCard extends LitElement {
 		return html`<ha-card .header="${this.config.title}"
 			>${content}</ha-card
 		>`;
+	}
+
+	renderTemplate(
+		str: Record<string, string> | string,
+	): Record<string, string> | string {
+		// Recursion!
+		if (typeof str == 'object' && str != null) {
+			for (const key in str) {
+				(str as Record<string, string>)[key] = this.renderTemplate(
+					(str as Record<string, string>)[key],
+				) as string;
+			}
+			return str;
+		}
+
+		// Define Home Assistant templating functions for nunjucks context
+		const hass = this.hass;
+
+		function states(entity_id: string) {
+			return hass.states[entity_id].state;
+		}
+
+		function is_state(entity_id: string, value: string | string[]) {
+			const state = states(entity_id);
+			if (typeof value == 'string') {
+				return state == value;
+			}
+			return value.includes(state);
+		}
+
+		function state_attr(entity_id: string, attribute: string) {
+			return hass.states[entity_id].attributes[attribute];
+		}
+
+		function is_state_attr(
+			entity_id: string,
+			attribute: string,
+			value: string | string[],
+		) {
+			const stateAttr = state_attr(entity_id, attribute);
+			if (typeof value == 'string') {
+				return stateAttr == value;
+			}
+			return value.includes(stateAttr);
+		}
+
+		function has_value(entity_id: string) {
+			try {
+				const state = states(entity_id);
+				if ([false, 0, -0, ''].includes(state)) {
+					return true;
+				} else {
+					return Boolean(state);
+				}
+			} catch {
+				return false;
+			}
+		}
+
+		const context = {
+			True: true,
+			False: false,
+			None: null,
+			states,
+			is_state,
+			state_attr,
+			is_state_attr,
+			has_value,
+		};
+
+		if (
+			typeof str == 'string' &&
+			(str.includes('{{') || str.includes('{%'))
+		) {
+			str = renderString(str, context).trim();
+		}
+
+		if (str == undefined || str == null) {
+			str = '';
+		}
+
+		return str;
 	}
 
 	static get styles() {
