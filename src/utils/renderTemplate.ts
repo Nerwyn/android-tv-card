d31730ebb3a8feabc4cebeb91a0abc4b36b447b9
Nerwyn Singh <nerwyn1994@gmail.com>
2023-12-02 19:55:38 -0500

    updated renderTemplate function

--- src/utils/renderTemplate.ts
+++ src/utils/renderTemplate.ts
@@ -2,48 +2,45 @@ import { HomeAssistant } from 'custom-card-helpers';
 import { renderString } from 'nunjucks';
 
 export function renderTemplate(hass: HomeAssistant, str: string): string {
-	// Recursion!
-	if (typeof str == 'object' && str != null) {
-		for (const key in str as Record<string, string>) {
-			(str as Record<string, string>)[key] = renderTemplate(
-				hass,
-				str[key],
-			);
+	if (
+		typeof str == 'string' &&
+		((str.includes('{{') && str.includes('}}')) ||
+			(str.includes('{%') && str.includes('%}')))
+	) {
+		const context = {
+			True: true,
+			False: false,
+			None: null,
+			states(entity_id: string) {
+				return _states(hass, entity_id);
+			},
+			is_state(entity_id: string, value: string) {
+				return _is_state(hass, entity_id, value);
+			},
+			state_attr(entity_id: string, attribute: string) {
+				return _state_attr(hass, entity_id, attribute);
+			},
+			is_state_attr(entity_id: string, attribute: string, value: string) {
+				return _is_state_attr(hass, entity_id, attribute, value);
+			},
+			has_value(entity_id: string) {
+				return _has_value(hass, entity_id);
+			},
+			iif(
+				condition: string,
+				if_true: string,
+				if_false?: string,
+				if_none?: string,
+			) {
+				return _iif(hass, condition, if_true, if_false, if_none);
+			},
+		};
+
+		str = renderString(structuredClone(str), context).trim();
+
+		if (str == undefined || str == null) {
+			str = '';
 		}
-		return str;
-	}
-
-	if (typeof str != 'string') {
-		return str;
-	}
-
-	const context = {
-		True: true,
-		False: false,
-		None: null,
-		states(entity_id: string) {
-			return _states(hass, entity_id);
-		},
-		is_state(entity_id: string, value: string) {
-			return _is_state(hass, entity_id, value);
-		},
-		state_attr(entity_id: string, attribute: string) {
-			return _state_attr(hass, entity_id, attribute);
-		},
-		is_state_attr(entity_id: string, attribute: string, value: string) {
-			return _is_state_attr(hass, entity_id, attribute, value);
-		},
-		has_value(entity_id: string) {
-			return _has_value(hass, entity_id);
-		},
-	};
-
-	if (str.includes('{{') || str.includes('{%')) {
-		str = renderString(str, context).trim();
-	}
-
-	if (str == undefined || str == null) {
-		str = '';
 	}
 
 	return str;
@@ -96,5 +93,28 @@ function _has_value(hass: HomeAssistant, entity_id: string) {
 		}
 	} catch {
 		return false;
+	}
+}
+
+function _iif(
+	hass: HomeAssistant,
+	condition: string,
+	if_true?: string,
+	if_false?: string,
+	if_none?: string,
+) {
+	const template = `
+		{% if ${condition} %}
+		${if_true ?? true}
+		{% else %}
+		${if_false ?? false}
+		{% endif %}
+	`;
+
+	const rendered = renderTemplate(hass, template);
+	if (rendered == '' && if_none) {
+		return if_none;
+	} else {
+		return rendered;
 	}
 }
