5bb449ee861e44bb05602bffa3567d2387e48f25
Nerwyn Singh <nerwyn1994@gmail.com>
2023-12-06 01:57:16 -0500

    update clean-tags

--- clean-tags.sh
+++ clean-tags.sh
@@ -1,2 +1,2 @@
-git push -d origin $(git tag -l "*dev*")
-git tag -d $(git tag -l "*dev*")
+git push -d origin $(git tag -l "*dev*" "*alpha*" "*beta*")
+git tag -d $(git tag -l "*dev*" "*alpha*" "*beta*")
