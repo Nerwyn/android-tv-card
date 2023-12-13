bca847a2148aa4b58541e877b307ac1a54705b84
Nerwyn Singh <nerwyn1994@gmail.com>
2023-12-13 18:06:41 -0500

    rename distributed file suffix to min.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
 	},
 	output: {
 		path: path.resolve(__dirname, './dist'),
-		filename: 'android-tv-card.js',
+		filename: 'android-tv-card.min.js',
 	},
 	resolve: {
 		extensions: ['.ts', '.tsx', '.js'],
