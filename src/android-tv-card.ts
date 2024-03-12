02daf46c9f1de7f28335e6e8306cdbb286d70550
Nerwyn Singh <nerwyn1994@gmail.com>
2024-03-12 12:36:42 -0400

    empty object coalesce custom actions slider in case it does not exist

--- src/android-tv-card.ts
+++ src/android-tv-card.ts
@@ -79,7 +79,7 @@ class AndroidTVCard extends LitElement {
 		this.customActions = config.custom_actions || {};
 		if (config.slider_id && !this.customActions.slider.tap_action) {
 			this.customActions.slider = {
-				...this.customActions.slider,
+				...(this.customActions.slider ?? {}),
 				...structuredClone(defaultKeys.slider),
 			};
 			this.customActions.slider.tap_action!.data!.entity_id =
