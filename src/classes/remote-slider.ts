befc6dc3f2e451831da29853405e6d727cdf2857
Nerwyn Singh <nerwyn1994@gmail.com>
2024-03-19 21:09:07 -0400

    undo onInput removal, does not work if using mousedown instead

--- src/classes/remote-slider.ts
+++ src/classes/remote-slider.ts
@@ -28,8 +28,7 @@ export class RemoteSlider extends BaseRemoteElement {
 	scrolling: boolean = false;
 	getValueFromHassTimer?: ReturnType<typeof setTimeout>;
 
-	@eventOptions({ passive: true })
-	onMouseDown(e: MouseEvent | TouchEvent) {
+	onInput(e: InputEvent) {
 		const slider = e.currentTarget as HTMLInputElement;
 
 		if (!this.scrolling) {
@@ -98,18 +97,18 @@ export class RemoteSlider extends BaseRemoteElement {
 		}
 	}
 
-	// @eventOptions({ passive: true })
-	// onMouseDown(e: MouseEvent | TouchEvent) {
-	// 	const slider = e.currentTarget as HTMLInputElement;
+	@eventOptions({ passive: true })
+	onMouseDown(e: MouseEvent | TouchEvent) {
+		const slider = e.currentTarget as HTMLInputElement;
 
-	// 	if (!this.scrolling) {
-	// 		this.getValueFromHass = false;
-	// 		clearTimeout(this.getValueFromHassTimer);
-	// 		this.value = slider.value;
-	// 		this.currentValue = slider.value;
-	// 		this.setTooltip(slider, true);
-	// 	}
-	// }
+		if (!this.scrolling) {
+			this.getValueFromHass = false;
+			clearTimeout(this.getValueFromHassTimer);
+			this.value = slider.value;
+			this.currentValue = slider.value;
+			this.setTooltip(slider, true);
+		}
+	}
 
 	onMouseUp(e: MouseEvent | TouchEvent) {
 		const slider = e.currentTarget as HTMLInputElement;
@@ -283,6 +282,7 @@ export class RemoteSlider extends BaseRemoteElement {
 				max="${this.range[1]}"
 				step=${this.step}
 				value="${value}"
+				@input=${this.onInput}
 				@mousedown=${this.onMouseDown}
 				@mouseup=${this.onMouseUp}
 				@mousemove=${this.onMouseMove}
