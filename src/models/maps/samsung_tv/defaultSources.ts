382271e859bbd1113c307a5cfd1ab522e82e2f99
Nerwyn Singh <nerwyn1994@gmail.com>
2024-10-03 19:39:18 -0400

    mention samsungtv custom integration in README and samsung_tv default sources

--- src/models/maps/samsung_tv/defaultSources.ts
+++ src/models/maps/samsung_tv/defaultSources.ts
@@ -1,5 +1,8 @@
 import { IElementConfig } from '../../interfaces';
 
+/**
+ * https://github.com/ollo69/ha-samsungtv-smart
+ */
 export const samsungTVDefaultSources: IElementConfig[] = [
 	{
 		type: 'button',
