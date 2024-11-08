ed4ba35d22c60585569f6b5e98d9293c092687a8
Nerwyn Singh <nerwyn1994@gmail.com>
2024-11-08 00:36:15 -0500

    restore file custom actions in autofill with missing cloning

--- src/universal-remote-card-editor.ts
+++ src/universal-remote-card-editor.ts
@@ -2512,7 +2512,10 @@ export class UniversalRemoteCardEditor extends LitElement {
 			if (parentName && childName) {
 				const parentActions =
 					structuredClone(
-						this.DEFAULT_ACTIONS.filter(
+						[
+							...(this.customActionsFromFile ?? []),
+							...this.DEFAULT_ACTIONS,
+						].filter(
 							(defaultActions) =>
 								defaultActions.name == parentName,
 						)[0],
@@ -2526,10 +2529,12 @@ export class UniversalRemoteCardEditor extends LitElement {
 			} else {
 				const actions =
 					structuredClone(
-						this.DEFAULT_ACTIONS.filter(
+						[
+							...(this.customActionsFromFile ?? []),
+							...this.DEFAULT_ACTIONS,
+						].filter(
 							(defaultActions) =>
-								defaultActions.name ==
-								this.renderTemplate(entry.name, context),
+								defaultActions.name == parentName,
 						)[0],
 					) ?? {};
 				entry = {
