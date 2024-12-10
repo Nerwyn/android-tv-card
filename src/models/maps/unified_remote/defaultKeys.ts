f0f09ec0587af6fd269cf800b183213c5e15f7f4
Nerwyn Singh <nerwyn1994@gmail.com>
2024-12-10 17:32:01 -0500

    add more unified remote default keys, README updates

--- src/models/maps/unified_remote/defaultKeys.ts
+++ src/models/maps/unified_remote/defaultKeys.ts
@@ -57,6 +57,29 @@ export const unifiedRemoteDefaultKeys: IElementConfig[] = [
 	},
 	{
 		type: 'button',
+		name: 'window_switcher',
+		tap_action: {
+			action: 'perform-action',
+			perform_action: 'unified_remote.call',
+			data: {
+				remote_id: 'Core.Input',
+				action: 'Stroke',
+				extras: {
+					Values: [
+						{
+							Value: 'lwin',
+						},
+						{
+							Value: 'tab',
+						},
+					],
+				},
+			},
+		},
+		icon: 'mdi:apps',
+	},
+	{
+		type: 'button',
 		name: 'back',
 		tap_action: {
 			action: 'perform-action',
@@ -534,5 +557,568 @@ export const unifiedRemoteDefaultKeys: IElementConfig[] = [
 			},
 		},
 		icon: 'mdi:magnify',
+	},
+	{
+		type: 'button',
+		name: 'n0',
+		tap_action: {
+			action: 'perform-action',
+			perform_action: 'unified_remote.call',
+			data: {
+				remote_id: 'Core.Input',
+				action: 'Press',
+				extras: {
+					Values: [
+						{
+							Value: '0',
+						},
+					],
+				},
+			},
+		},
+		icon: 'mdi:numeric-0',
+	},
+	{
+		type: 'button',
+		name: 'n1',
+		tap_action: {
+			action: 'perform-action',
+			perform_action: 'unified_remote.call',
+			data: {
+				remote_id: 'Core.Input',
+				action: 'Press',
+				extras: {
+					Values: [
+						{
+							Value: '1',
+						},
+					],
+				},
+			},
+		},
+		icon: 'mdi:numeric-1',
+	},
+	{
+		type: 'button',
+		name: 'n2',
+		tap_action: {
+			action: 'perform-action',
+			perform_action: 'unified_remote.call',
+			data: {
+				remote_id: 'Core.Input',
+				action: 'Press',
+				extras: {
+					Values: [
+						{
+							Value: '2',
+						},
+					],
+				},
+			},
+		},
+		icon: 'mdi:numeric-2',
+	},
+	{
+		type: 'button',
+		name: 'n3',
+		tap_action: {
+			action: 'perform-action',
+			perform_action: 'unified_remote.call',
+			data: {
+				remote_id: 'Core.Input',
+				action: 'Press',
+				extras: {
+					Values: [
+						{
+							Value: '3',
+						},
+					],
+				},
+			},
+		},
+		icon: 'mdi:numeric-3',
+	},
+	{
+		type: 'button',
+		name: 'n4',
+		tap_action: {
+			action: 'perform-action',
+			perform_action: 'unified_remote.call',
+			data: {
+				remote_id: 'Core.Input',
+				action: 'Press',
+				extras: {
+					Values: [
+						{
+							Value: '4',
+						},
+					],
+				},
+			},
+		},
+		icon: 'mdi:numeric-4',
+	},
+	{
+		type: 'button',
+		name: 'n5',
+		tap_action: {
+			action: 'perform-action',
+			perform_action: 'unified_remote.call',
+			data: {
+				remote_id: 'Core.Input',
+				action: 'Press',
+				extras: {
+					Values: [
+						{
+							Value: '5',
+						},
+					],
+				},
+			},
+		},
+		icon: 'mdi:numeric-5',
+	},
+	{
+		type: 'button',
+		name: 'n6',
+		tap_action: {
+			action: 'perform-action',
+			perform_action: 'unified_remote.call',
+			data: {
+				remote_id: 'Core.Input',
+				action: 'Press',
+				extras: {
+					Values: [
+						{
+							Value: '6',
+						},
+					],
+				},
+			},
+		},
+		icon: 'mdi:numeric-6',
+	},
+	{
+		type: 'button',
+		name: 'n7',
+		tap_action: {
+			action: 'perform-action',
+			perform_action: 'unified_remote.call',
+			data: {
+				remote_id: 'Core.Input',
+				action: 'Press',
+				extras: {
+					Values: [
+						{
+							Value: '7',
+						},
+					],
+				},
+			},
+		},
+		icon: 'mdi:numeric-7',
+	},
+	{
+		type: 'button',
+		name: 'n8',
+		tap_action: {
+			action: 'perform-action',
+			perform_action: 'unified_remote.call',
+			data: {
+				remote_id: 'Core.Input',
+				action: 'Press',
+				extras: {
+					Values: [
+						{
+							Value: '8',
+						},
+					],
+				},
+			},
+		},
+		icon: 'mdi:numeric-8',
+	},
+	{
+		type: 'button',
+		name: 'n9',
+		tap_action: {
+			action: 'perform-action',
+			perform_action: 'unified_remote.call',
+			data: {
+				remote_id: 'Core.Input',
+				action: 'Press',
+				extras: {
+					Values: [
+						{
+							Value: '9',
+						},
+					],
+				},
+			},
+		},
+		icon: 'mdi:numeric-9',
+	},
+	{
+		type: 'button',
+		name: 'numpad',
+		icon: 'mdi:dialpad',
+	},
+	{
+		type: 'button',
+		name: 'f1',
+		tap_action: {
+			action: 'perform-action',
+			perform_action: 'unified_remote.call',
+			data: {
+				remote_id: 'Core.Input',
+				action: 'Press',
+				extras: {
+					Values: [
+						{
+							Value: 'f1',
+						},
+					],
+				},
+			},
+		},
+		icon: 'mdi:keyboard-f1',
+	},
+	{
+		type: 'button',
+		name: 'f2',
+		tap_action: {
+			action: 'perform-action',
+			perform_action: 'unified_remote.call',
+			data: {
+				remote_id: 'Core.Input',
+				action: 'Press',
+				extras: {
+					Values: [
+						{
+							Value: 'f2',
+						},
+					],
+				},
+			},
+		},
+		icon: 'mdi:keyboard-f2',
+	},
+	{
+		type: 'button',
+		name: 'f3',
+		tap_action: {
+			action: 'perform-action',
+			perform_action: 'unified_remote.call',
+			data: {
+				remote_id: 'Core.Input',
+				action: 'Press',
+				extras: {
+					Values: [
+						{
+							Value: 'f3',
+						},
+					],
+				},
+			},
+		},
+		icon: 'mdi:keyboard-f3',
+	},
+	{
+		type: 'button',
+		name: 'f4',
+		tap_action: {
+			action: 'perform-action',
+			perform_action: 'unified_remote.call',
+			data: {
+				remote_id: 'Core.Input',
+				action: 'Press',
+				extras: {
+					Values: [
+						{
+							Value: 'f4',
+						},
+					],
+				},
+			},
+		},
+		icon: 'mdi:keyboard-f4',
+	},
+	{
+		type: 'button',
+		name: 'f5',
+		tap_action: {
+			action: 'perform-action',
+			perform_action: 'unified_remote.call',
+			data: {
+				remote_id: 'Core.Input',
+				action: 'Press',
+				extras: {
+					Values: [
+						{
+							Value: 'f5',
+						},
+					],
+				},
+			},
+		},
+		icon: 'mdi:keyboard-f5',
+	},
+	{
+		type: 'button',
+		name: 'f6',
+		tap_action: {
+			action: 'perform-action',
+			perform_action: 'unified_remote.call',
+			data: {
+				remote_id: 'Core.Input',
+				action: 'Press',
+				extras: {
+					Values: [
+						{
+							Value: 'f6',
+						},
+					],
+				},
+			},
+		},
+		icon: 'mdi:keyboard-f6',
+	},
+	{
+		type: 'button',
+		name: 'f7',
+		tap_action: {
+			action: 'perform-action',
+			perform_action: 'unified_remote.call',
+			data: {
+				remote_id: 'Core.Input',
+				action: 'Press',
+				extras: {
+					Values: [
+						{
+							Value: 'f7',
+						},
+					],
+				},
+			},
+		},
+		icon: 'mdi:keyboard-f7',
+	},
+	{
+		type: 'button',
+		name: 'f8',
+		tap_action: {
+			action: 'perform-action',
+			perform_action: 'unified_remote.call',
+			data: {
+				remote_id: 'Core.Input',
+				action: 'Press',
+				extras: {
+					Values: [
+						{
+							Value: 'f8',
+						},
+					],
+				},
+			},
+		},
+		icon: 'mdi:keyboard-f8',
+	},
+	{
+		type: 'button',
+		name: 'f9',
+		tap_action: {
+			action: 'perform-action',
+			perform_action: 'unified_remote.call',
+			data: {
+				remote_id: 'Core.Input',
+				action: 'Press',
+				extras: {
+					Values: [
+						{
+							Value: 'f9',
+						},
+					],
+				},
+			},
+		},
+		icon: 'mdi:keyboard-f9',
+	},
+	{
+		type: 'button',
+		name: 'f10',
+		tap_action: {
+			action: 'perform-action',
+			perform_action: 'unified_remote.call',
+			data: {
+				remote_id: 'Core.Input',
+				action: 'Press',
+				extras: {
+					Values: [
+						{
+							Value: 'f10',
+						},
+					],
+				},
+			},
+		},
+		icon: 'mdi:keyboard-f10',
+	},
+	{
+		type: 'button',
+		name: 'f11',
+		tap_action: {
+			action: 'perform-action',
+			perform_action: 'unified_remote.call',
+			data: {
+				remote_id: 'Core.Input',
+				action: 'Press',
+				extras: {
+					Values: [
+						{
+							Value: 'f11',
+						},
+					],
+				},
+			},
+		},
+		icon: 'mdi:keyboard-f11',
+	},
+	{
+		type: 'button',
+		name: 'f12',
+		tap_action: {
+			action: 'perform-action',
+			perform_action: 'unified_remote.call',
+			data: {
+				remote_id: 'Core.Input',
+				action: 'Press',
+				extras: {
+					Values: [
+						{
+							Value: 'f12',
+						},
+					],
+				},
+			},
+		},
+		icon: 'mdi:keyboard-f12',
+	},
+	{
+		type: 'button',
+		name: 'dock-top',
+		tap_action: {
+			action: 'perform-action',
+			perform_action: 'unified_remote.call',
+			data: {
+				remote_id: 'Core.Input',
+				action: 'Stroke',
+				extras: {
+					Values: [
+						{
+							Value: 'lwin',
+						},
+						{
+							Value: 'up',
+						},
+					],
+				},
+			},
+		},
+		icon: 'mdi:dock-top',
+	},
+	{
+		type: 'button',
+		name: 'dock-bottom',
+		tap_action: {
+			action: 'perform-action',
+			perform_action: 'unified_remote.call',
+			data: {
+				remote_id: 'Core.Input',
+				action: 'Stroke',
+				extras: {
+					Values: [
+						{
+							Value: 'lwin',
+						},
+						{
+							Value: 'down',
+						},
+					],
+				},
+			},
+		},
+		icon: 'mdi:dock-bottom',
+	},
+	{
+		type: 'button',
+		name: 'dock-left',
+		tap_action: {
+			action: 'perform-action',
+			perform_action: 'unified_remote.call',
+			data: {
+				remote_id: 'Core.Input',
+				action: 'Stroke',
+				extras: {
+					Values: [
+						{
+							Value: 'lwin',
+						},
+						{
+							Value: 'left',
+						},
+					],
+				},
+			},
+		},
+		icon: 'mdi:dock-left',
+	},
+	{
+		type: 'button',
+		name: 'dock-right',
+		tap_action: {
+			action: 'perform-action',
+			perform_action: 'unified_remote.call',
+			data: {
+				remote_id: 'Core.Input',
+				action: 'Stroke',
+				extras: {
+					Values: [
+						{
+							Value: 'lwin',
+						},
+						{
+							Value: 'right',
+						},
+					],
+				},
+			},
+		},
+		icon: 'mdi:dock-right',
+	},
+	{
+		type: 'button',
+		name: 'zoom_in',
+		tap_action: {
+			action: 'perform-action',
+			perform_action: 'unified_remote.call',
+			data: {
+				remote_id: 'Core.Input',
+				action: 'ZoomIn',
+			},
+		},
+		icon: 'mdi:magnify-plus',
+	},
+	{
+		type: 'button',
+		name: 'zoom_out',
+		tap_action: {
+			action: 'perform-action',
+			perform_action: 'unified_remote.call',
+			data: {
+				remote_id: 'Core.Input',
+				action: 'ZoomOut',
+			},
+		},
+		icon: 'mdi:magnify-minus',
 	},
 ];
